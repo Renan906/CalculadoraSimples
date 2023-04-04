49f396a1d265715894bf5a54644a6e5e4639baa0
Renan Rezende Rolão <82110641+Renan906@users.noreply.github.com>
2023-04-03 23:50:49 -0300

    Add files via upload

---
+++ src/calculadorajava/CalculadoraJava.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadorajava;
+
+/**
+ *
+ * @author Renan Rezende
+ */
+public class CalculadoraJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MenuCalculadora menu = new MenuCalculadora();
+        menu.setVisible(true);
+        
+        
+    }
+    
+}
---
+++ src/calculadorajava/Operaçoes.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadorajava;
+
+/**
+ *
+ * @author Renan Rezende
+ */
+public class Operaçoes {
+    
+    public float somar (float numero1, float numero2){
+    return numero1 + numero2;
+    }
+    public float subtrair (float numero1, float numero2){
+    return numero1 - numero2;
+    }
+    public float multiplicar (float numero1, float numero2){
+    return numero1 * numero2;
+    }
+    public float dividir (float numero1, float numero2){
+    return numero1 / numero2;
+    }
+}
